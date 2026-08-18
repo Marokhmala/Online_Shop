e21f78e94ad0bfbf9b37ab9e4f7f3a3bf5a03990
Mariami <maria.kh.2073@gmail.com>
2026-08-18 21:41:43 +0400

    modified the website display of a test product, plus started working on cart

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView, add_to_cart
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('product/<int:product_id>/add-to-cart/', add_to_cart, name='add-to-cart'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,39 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.db.models import Avg
 from .models import Product
+from django.shortcuts import redirect, get_object_or_404
 
 
 class ProductListView(ListView):
     model = Product
     template_name = 'store/product_list.html'
     context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.annotate(
+            average_rating=Avg('reviews__rating')
+        )
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'store/product_detail.html'
+    context_object_name = 'product'
+
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    cart = request.session.get('cart', {})
+
+    product_id = str(product.id)
+
+    if product_id in cart:
+        cart[product_id] += 1
+    else:
+        cart[product_id] = 1
+
+    request.session['cart'] = cart
+    request.session.modified = True
+
+    return redirect('product-list')
